d0b1be9887f0c29f11da6bf9f65327b4ff1c3139
Felix Brucker <felix@felixbrucker.com>
2026-08-18 18:13:10 +1200

    Fix currency symbol left shift

--- app/src/main/java/com/felixbrucker/currencyconverter/ui/components/ConversionCard.kt
+++ app/src/main/java/com/felixbrucker/currencyconverter/ui/components/ConversionCard.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -252,7 +254,8 @@ fun ConversionCard(
                             ),
                             decorationBox = { innerTextField ->
                                 Box(
-                                    contentAlignment = Alignment.CenterEnd
+                                    contentAlignment = Alignment.CenterEnd,
+                                    modifier = Modifier.width(IntrinsicSize.Min),
                                 ) {
                                     if (rowState.enteredText.isEmpty()) {
                                         Text(
