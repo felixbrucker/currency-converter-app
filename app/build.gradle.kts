aeaa5271005eb88eb11bdb9d1e9b0bce8c8a7b9b
Felix Brucker <felix@felixbrucker.com>
2026-08-30 20:56:28 +1200

    Prepare release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     applicationId = "com.felixbrucker.currencyconverter"
     minSdk = 26
     targetSdk = 37
-    versionCode = 6
-    versionName = "1.1.0"
+    versionCode = 7
+    versionName = "1.1.1"
   }
 
   signingConfigs {
